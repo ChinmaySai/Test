d031caf895db706307918f752a65982c521de8af
ChinmaySai <yanamalachinmaysai106@gmail.com>
2024-10-17 22:50:52 +0530

    Java 8

---
+++ Java8/src/Lamda.java
@@ -0,0 +1,14 @@
+interface Functional{
+	void m1();
+}
+public class Lamda{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Functional l= () -> System.out.println("Hi");
+	       l.m1();
+
+	}
+    
+	
+}
